3af7d256f5a4001a1b5068fb0516d188c058ea9a
Arthur <arthur91021@gmail.com>
2025-03-04 18:24:10 -0800

    Adding fake authentication: Protecting a route

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import City from "./Components/City";
 import Form from "./Components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -25,7 +26,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,12 @@ import Sidebar from "../Components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../Components/Map";
 import User from "../Components/User";
+import ProtectedRoute from "./ProtectedRoute";
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
-
       <User />
     </div>
   );
